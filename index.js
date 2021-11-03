5fb2fce42ca10d80c382c66a2824ba249b7e47ab
Steve Ripberger <sripberger@batterii.com>
2021-11-03 14:00:01 -0400

    Override naming-convention for UPPER_CASE constants

--- index.js
+++ index.js
@@ -15,11 +15,50 @@ module.exports = {
 		"react-hooks",
 	],
 	rules: {
+		"@typescript-eslint/naming-convention": [
+			"error",
+			/* eslint-disable sort-keys */
+			{
+				selector: "default",
+				format: ["camelCase", "PascalCase"],
+			},
+			{
+				selector: "parameter",
+				format: ["camelCase", "PascalCase"],
+				leadingUnderscore: "allow",
+			},
+			{
+				selector: "memberLike",
+				modifiers: ["private"],
+				format: ["camelCase"],
+				leadingUnderscore: "require",
+			},
+			{
+				selector: "memberLike",
+				modifiers: ["protected"],
+				format: ["camelCase"],
+				leadingUnderscore: "require",
+			},
+			{
+				selector: ["typeLike", "enumMember"],
+				format: ["PascalCase"],
+			},
+			{
+				selector: "typeParameter",
+				format: ["PascalCase"],
+			},
+			{
+				selector: "const",
+				format: ["UPPER_CASE"],
+			},
+			/* eslint-enable sort-keys */
+		],
 		"@typescript-eslint/no-extra-parens": "off",
 		"multiline-ternary": ["error", "always-multiline"],
 		"no-nested-ternary": "off",
 		"react-hooks/rules-of-hooks": "error",
 		"react/boolean-prop-naming": "error",
+
 	},
 	settings: {
 		react: {version: "detect"},
